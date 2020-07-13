62a6ce3c0649ef1b68fe64f2b8bba8708cccd813
Dubinkin Pavel <niknibud@gmail.com>
2020-07-13 20:54:40 +0300

    Добавил функцию renderStatistics

--- js/stats.js
+++ js/stats.js
@@ -1,6 +1,9 @@
 'use strict';
 
 window.renderStatistics = function (ctx) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+  ctx.fillRect(110, 60, 500, 200);
+
   ctx.fillStyle = '#fff';
   ctx.fillRect(100, 50, 500, 200);
 };
