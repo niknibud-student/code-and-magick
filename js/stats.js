a0540b8d525bfd6016b427cb82296993aadb2c12
Dubinkin Pavel <niknibud@gmail.com>
2020-07-13 21:27:03 +0300

    Расчёт положения гистограммы

--- js/stats.js
+++ js/stats.js
@@ -1,9 +1,32 @@
 'use strict';
 
-window.renderStatistics = function (ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
-  ctx.fillRect(110, 60, 500, 200);
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 200;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
 
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 50, 500, 200);
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+
+  ctx.fillText('Иван', 110, 105);
+  ctx.fillRect(160, 90, 430, 20);
+
+  ctx.fillText('Юлия', 110, 135);
+  ctx.fillRect(160, 120, 430, 20);
 };
